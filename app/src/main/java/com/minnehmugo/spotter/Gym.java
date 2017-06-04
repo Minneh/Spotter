5533f2d227dafad4ac5ec6120af110ba36e2b15a
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-04 21:24:44 +0300

    add gym model and list view

---
+++ app/src/main/java/com/minnehmugo/spotter/Gym.java
@@ -0,0 +1,75 @@
+package com.minnehmugo.spotter;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by minnehmugo on 04/06/2017.
+ */
+@Parcel
+public class Gym {
+    String mName;
+    String mPhone;
+    String mWebsite;
+    double mRating;
+    String mImageUrl;
+    ArrayList<String> mAddress = new ArrayList<>();
+    double mLatitude;
+    double mLongitude;
+    ArrayList<String> mCategories = new ArrayList<>();
+
+    // empty constructor needed by the Parceler library:
+    public Gym() {}
+
+    public Gym(String name, String phone, String website,
+                      double rating, String imageUrl, ArrayList<String> address,
+                      double latitude, double longitude, ArrayList<String> categories) {
+        this.mName = name;
+        this.mPhone = phone;
+        this.mWebsite = website;
+        this.mRating = rating;
+        this.mImageUrl = imageUrl;
+        this.mAddress = address;
+        this.mLatitude = latitude;
+        this.mLongitude = longitude;
+        this.mCategories = categories;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPhone() {
+        return mPhone;
+    }
+
+    public String getWebsite() {
+        return  mWebsite;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public String getImageUrl(){
+        return mImageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return mAddress;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public ArrayList<String> getCategories() {
+        return mCategories;
+    }
+}
+
--- app/src/main/java/com/minnehmugo/spotter/GymListActivity.java
+++ app/src/main/java/com/minnehmugo/spotter/GymListActivity.java
@@ -8,6 +8,6 @@ public class GymListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gym_list);
+        setContentView(R.layout.gym_list_item);
     }
 }
