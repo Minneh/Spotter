32f2a08224324319ad0ed3a4958a6dc7fac9e339
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-04 21:11:13 +0300

    create main activity interface and back-end

--- app/src/main/java/com/minnehmugo/spotter/MainActivity.java
+++ app/src/main/java/com/minnehmugo/spotter/MainActivity.java
@@ -1,13 +1,41 @@
 package com.minnehmugo.spotter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findGymsButton) Button mFindGymsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface kaushanScript = Typeface.createFromAsset(getAssets(), "fonts/KaushanScript-Regular.otf");
+        mAppNameTextView.setTypeface(kaushanScript);
+
+        mFindGymsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindGymsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, GymListActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
