286a7d7c768e851da700e131530729f83234caa4
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-08 15:29:19 +0300

    add white search icons to drawable folders

--- app/src/main/java/com/minnehmugo/spotter/ui/LoginActivity.java
+++ app/src/main/java/com/minnehmugo/spotter/ui/LoginActivity.java
@@ -1,26 +1,16 @@
 package com.minnehmugo.spotter.ui;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.minnehmugo.spotter.R;
 
-import butterknife.Bind;
-
 public class LoginActivity extends AppCompatActivity {
-
-    @Bind(R.id.loginTextView)
-    TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Typeface kaushanScript = Typeface.createFromAsset(getAssets(), "fonts/KaushanScript-Regular.otf");
-        mLoginTextView.setTypeface(kaushanScript);
 
     }
 }
