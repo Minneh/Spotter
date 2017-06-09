b13f4571c8bad99cf926e55f432ad90efdaa528e
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-09 12:52:34 +0300

    modify loginWithPassword() - add OnCompleteListener responsible for determining when the authentication attempt is complete and executing the onComplete() override when it is

--- app/src/main/java/com/minnehmugo/spotter/ui/LoginActivity.java
+++ app/src/main/java/com/minnehmugo/spotter/ui/LoginActivity.java
@@ -2,12 +2,18 @@ package com.minnehmugo.spotter.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.minnehmugo.spotter.R;
 
@@ -58,5 +64,20 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             mPasswordEditText.setError("Password cannot be blank");
             return;
         }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
     }
 }
